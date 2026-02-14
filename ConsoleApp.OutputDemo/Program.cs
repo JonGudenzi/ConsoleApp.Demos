39b21dd8ba9f520d1dc80b246741eccba7289014
Jon Gudenzi <jongudenzi@gmail.com>
2026-02-14 13:19:14 -0500

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World! My name is Jon");
+
+Console.WriteLine("I am learning C# and .NET 10.0");
+
+Console.Write("Line 1, ");
+Console.Write("Line 2");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿//Different DataTypes
+/*
+text - string
+integer - int
+decimal - double, float, decimal
+logical - bool
+*/
+
+
+string name = "Jon";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // String concatenation
+Console.WriteLine($"My name is {name}");// String interpolation
+Console.WriteLine( "{0} is my name", name);// string formatting
+
+int age = 43;
+int retirementYearsLeft = 24;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine("My age is: " + age);
+
+Console.WriteLine("My retirement age is " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
