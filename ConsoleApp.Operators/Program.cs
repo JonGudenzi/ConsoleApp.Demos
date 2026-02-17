3d00cfbd5626ec7166be8261ed1af6a229e79900
Jon Gudenzi <jongudenzi@gmail.com>
2026-02-16 21:55:11 -0500

    Switch statement lesson

--- ConsoleApp.Operators/Program.cs
+++ ConsoleApp.Operators/Program.cs
@@ -77,3 +77,41 @@ num1 *= randomValue;
 Console.WriteLine($"num 1 multiplied by {randomValue}: {num1}");
 Console.WriteLine();
 Console.WriteLine(("-------- End Assignment Results ---------"));
+
+
+Console.WriteLine(("-------- Switch Statement ---------"));
+Console.WriteLine();
+
+Console.Write("Please enter the day of the week: ");
+
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+switch (dayOfWeek)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.Write("invalid day number entered");
+        break;
+}
+
+Console.WriteLine();
+Console.WriteLine(("-------- End Switch Statement ---------"));
