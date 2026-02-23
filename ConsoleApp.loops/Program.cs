be9ee73b0c5553c5af8764484bda4d8fc1aad37f
Jon Gudenzi <jongudenzi@gmail.com>
2026-02-22 21:52:43 -0500

    for loop lesson

---
+++ ConsoleApp.loops/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+//Ask user how many times they want to print "Hello world"
+
+Console.WriteLine("How many times do you want to print 'Hello world!'? ");
+
+int userCount = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < userCount; i++)
+{
+    Console.WriteLine($"Hello world! - {i}");
+}
+
+
+
+
+
