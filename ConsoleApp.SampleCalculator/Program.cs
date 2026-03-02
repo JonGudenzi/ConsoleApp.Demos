3aaa9852fa81a96f19373af6a90945dcff455921
Jon Gudenzi <jongudenzi@gmail.com>
2026-03-01 20:32:56 -0500

    Refactor calculator using Exceptions

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,27 +1,55 @@
 ﻿//Welcome Message
 
-using System.ComponentModel.DataAnnotations;
 
-Console.WriteLine(" ------- Welcome to the Sample Calculator! --------- ");
+
+//Variable Declarations
+int choice = 0;
+int num1 = 0;
+int num2 = 0;
+
 // Show calulator options / Show menu
-Console.WriteLine("Please select an operation: ( -1 to exit )");
-Console.WriteLine("1. Addition");
-Console.WriteLine("2. Subtraction");
-Console.WriteLine("3. Multiplication");
-Console.WriteLine("4. Division");
-Console.WriteLine("5. Fibonacci sequence");
-int choice = Convert.ToInt32(Console.ReadLine());
-
 while (choice != -1)
 {
-    //Promt for user input
-    Console.WriteLine("Please enter the first number: ");
-    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ------- Welcome to the Sample Calculator! --------- ");
+    Console.WriteLine("Please select an operation: ( -1 to exit )");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Fibonacci sequence");
 
-    Console.WriteLine("Please enter the second number: ");
-    int num2 = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Invalid Input, press any key to try again. ");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
 
+    if (choice == -1)
+    {
+        break;
+    }
 
+    try
+    {
+        Console.WriteLine("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Invalid Input, press any key to try again. ");
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
 
     // Decide which operation is needed based on selected option
     int answer = 0;
@@ -37,8 +65,24 @@ while (choice != -1)
             answer = num1 * num2;
             break;
         case 4:
-            answer = num1 / num2;
+            try
+            {
+                answer = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                Console.WriteLine("Press Endter to continue");
+                Console.ReadLine();
+                Console.Clear();
+                continue;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
             break;
+
         case 5:
             for (int i = num1; i <= num2; i++)
             {
@@ -46,22 +90,11 @@ while (choice != -1)
             }
             break;
         default:
-            Console.WriteLine("Invalid choice. Please select a valid operation.");
-            break;
+            Console.WriteLine("Invalid choice");
+            Console.WriteLine("Press Endter to continue");
+            Console.ReadLine();
+            continue;
     }
-
-    // Print output
-    Console.WriteLine($"The result is : {answer}");
-    Console.WriteLine("Press enter to continue");
-    Console.ReadLine();
-    Console.Clear();
-    Console.WriteLine("Please select an operation: ( -1 to exit )");
-    Console.WriteLine("1. Addition");
-    Console.WriteLine("2. Subtraction");
-    Console.WriteLine("3. Multiplication");
-    Console.WriteLine("4. Division");
-    Console.WriteLine("5. Fibonacci sequence");
-    choice = Convert.ToInt32(Console.ReadLine());
 }
 Console.WriteLine(" ------- Thank you for using the simple calculator ------- ");
 
